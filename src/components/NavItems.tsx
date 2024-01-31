ed56d738d8fd600802532f74b15c9da70b911fc0
jonathanferreirar <jonathanferreirar@gmail.com>
2024-01-31 01:24:15 -0300

    :sparkles: feat: bulding a complete format price currency function

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -2,11 +2,24 @@
 
 import { PRODUCT_CATEGORIES } from '@/config'
 import NavItem from './NavItem'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useOnClickOutside } from '@/hooks/use-on-clik-outside'
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+    document.addEventListener('keydown', handler)
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [])
 
   const isAnyOpen = activeIndex !== null
 
